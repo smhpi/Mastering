9e345fd22ce7348114b6014b0a01ae6b6ee0c02a
Hosen Poursaeid <poursaeid@outlook.com>
2019-01-29 10:09:25 -0500

    longestWord

---
+++ longestWord.js
@@ -0,0 +1,13 @@
+function longestWord(str) {
+  var array1 = str.match(/\w[a-z]{0,}/gi);
+  var result = array1[0];
+
+  for (var x = 1; x < array1.length; x++) {
+    if (result.length < array1[x].length) {
+      result = array1[x];
+    }
+  }
+  return result;
+}
+
+console.log(longestWord("Using the JavaScript language, have the function"));
